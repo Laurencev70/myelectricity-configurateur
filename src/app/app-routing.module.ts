ef055a124ad71a19def91aa9627cdd92dcbfcf60
Laurence VADORI <laurencevadori@MacBook-Pro-de-Laurence.local>
2020-04-07 23:35:26 +0200

    location-component-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LocationComponent } from './location/location.component';
 
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'location', component: LocationComponent},
 ];
 
 @NgModule({
